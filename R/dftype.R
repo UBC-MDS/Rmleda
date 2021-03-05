30747d59840af86707c15cbe4dcb26eedf88fa18
Saule Atymtayeva <68589685+Saule-Atymtayeva@users.noreply.github.com>
2021-03-05 11:50:38 -0800

    Update R/dftype.R

--- R/dftype.R
+++ R/dftype.R
@@ -1,7 +1,7 @@
 #' Explore the type of data frame variables and columns.
 #'
 #' Create a new data frame which contians the type of columns and variables for the input data frame.
-#'  Furthermore, if there are non-numeric columns, it will return the unique values of non-numeric columns and their length.
+#' Furthermore, if there are non-numeric columns, it will return the unique values of non-numeric columns and their length.
 #'
 #' @param df Data Frame
 #'
