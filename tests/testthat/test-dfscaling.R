fa58bca0a07db8aa453c193c245f94345a2a96aa
Tanmay Sharma <tanmaysharma19@gmail.com>
2021-03-11 18:28:07 -0800

    Update dfscaling function call in tests

--- tests/testthat/test-dfscaling.R
+++ tests/testthat/test-dfscaling.R
@@ -1,18 +1,17 @@
 df_1 <- tibble(
-  x = c(0, 0, 1, 1),
+  x = c(0, 0, 2, 2),
   y = c(2, 2, 4, 4),
   z = 5,
-  target = c(1,1,0,1,0)
+  target = c(1,1,0,1)
 )
 
 df_result <- tibble(
-  x = c(-1.0, -1.0, 1.0, 1.0),
-  y = c(-1.0, -1.0, 1.0, 1.0),
-  Class = as.factor(c(1,1,0,1,0))
+  x = c(-0.866, -0.866, 0.866, 0.866),
+  y = c(-0.866, -0.866, 0.866, 0.866),
+  Class = as.factor(c(1,1,0,1))
 )
 
 
-
 test_that("dfscaling works", {
-  expect_equal(2 * 2, 4)
+  expect_true(all.equal(Rmleda::dfscaling(df_1, target), df_result, tolerance = 0.01))
 })
