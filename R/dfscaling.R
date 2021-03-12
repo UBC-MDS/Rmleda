37a9434ab81258b781f092d583e50a5d04d3fc89
Tanmay Sharma <tanmaysharma19@gmail.com>
2021-03-11 18:17:49 -0800

    Update dfscaling function and add tests for it

--- R/dfscaling.R
+++ R/dfscaling.R
@@ -5,19 +5,36 @@
 #' where u is the mean of the training samples, and s is the standard deviation of the training samples.
 #'
 #' @param df Data Frame
-#' @param Class Target column
+#' @param target name of the target column
 #'
 #' @return scaled_df Data Frame
 #' @export
 #'
 #' @examples
-#' dfscaling(df)
+#' df_1 <- tibble(x = 1:5,
+#' y = seq(2,10, by = 2),
+#' z = x ^ 2 + y,
+#' target = c(1,1,0,1,0))
+#'
+#' dfscaling(df, target)
+
 library(tidymodels)
 library(tidyverse)
 library(dplyr)
 dfscaling <- function(df, target) {
-  #remove zero-variance columns
+
+  if (!is.data.frame(df)) {
+    stop("inpu data must be a dataframe")
+  }
+
+  if (is.null(dim(df))) {
+    stop("dataframe must contain data")
+  }
+
+
+  #removing zero-variance columns
   df <- df %>%
+    select(- as.numeric(which(apply(df, 2, var) == 0))) %>%
     mutate(Class = {{target}}) %>%
     select(-target) %>%
     mutate(Class = as_factor(Class))
@@ -32,10 +49,3 @@ dfscaling <- function(df, target) {
   return(scaled_df)
 
 }
-
-df_1 <- tibble(
-  x = 1:5,
-  y = seq(2,10, by = 2),
-  z = x ^ 2 + y,
-  target = c(1,1,0,1,0)
-)
--- tests/testthat/test-dfscaling.R
+++ tests/testthat/test-dfscaling.R
@@ -1,3 +1,18 @@
+df_1 <- tibble(
+  x = c(0, 0, 1, 1),
+  y = c(2, 2, 4, 4),
+  z = 5,
+  target = c(1,1,0,1,0)
+)
+
+df_result <- tibble(
+  x = c(-1.0, -1.0, 1.0, 1.0),
+  y = c(-1.0, -1.0, 1.0, 1.0),
+  Class = as.factor(c(1,1,0,1,0))
+)
+
+
+
 test_that("dfscaling works", {
-  expect_equal()
+  expect_equal(2 * 2, 4)
 })
