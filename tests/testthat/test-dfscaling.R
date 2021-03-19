7e63e74f987398f3f63779a58106cfc7645de9e1
Tanmay Sharma <tanmaysharma19@gmail.com>
2021-03-18 17:04:35 -0700

    Update test coverage to 100%

--- tests/testthat/test-dfscaling.R
+++ tests/testthat/test-dfscaling.R
@@ -11,6 +11,16 @@ df_result <- tidyr::tibble(
   Class = as.factor(c(1,1,0,1))
 )
 
+df_empty <- tidyr::tibble()
+
+
+test_that("dfscaling checks that input data must be of type dataframe", {
+  expect_error(dfscaling("I'm not a dataframe", target))
+})
+
+test_that("dfscaling checks for empty dataframes", {
+  expect_error(dfscaling(df_empty, target))
+})
 
 test_that("dfscaling works", {
   expect_true(all.equal(dfscaling(df_1, target), df_result, tolerance = 0.01))
